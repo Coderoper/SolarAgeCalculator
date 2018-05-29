f18ff4cfdab991e011cefaad060722e32cf73192
Guest User <Guest@epicodus-39.local>
2018-05-29 09:24:46 -0700

    Fixed the compile errors

--- spec/solar-age-spec.js
+++ spec/solar-age-spec.js
@@ -7,31 +7,32 @@ describe('SolarAge', function(){
     expect(TestSolarAge.YearBorn).toEqual(2000);
 
   });
-  it('should test whether VenusAge method returns a string'){
+  it('should test whether VenusAge method returns a string', function(){
 
     var TestSolarAge = new SolarAge(1918, 5, 25, 100);
     expect(TestSolarAge.VenusAge()).toEqual("Venus Age")
-  }
-  it('should test whether MarsAge method returns a string'){
+  });
+  it('should test whether MarsAge method returns a string', function(){
 
     var TestSolarAge = new SolarAge(1918, 5, 25, 100);
     expect(TestSolarAge.MarsAge()).toEqual("Mars Age")
-  }
-  it('should test whether MercuryAge method returns a string'){
+  });
+  it('should test whether MercuryAge method returns a string', function(){
 
     var TestSolarAge = new SolarAge(1918, 5, 25, 100);
     expect(TestSolarAge.MercuryAge()).toEqual("Mercury Age")
-  }
-  it('should teest Years to live'){
+  });
+  it('should teest Years to live', function(){
     var TestSolarAge = new SolarAge(1918, 5, 25, 100);
     console.log(TestSolarAge.DeathAge);
     expect(TestSolarAge.YearsLeftToLive()).toEqual("several more years")
 
-  }
-  it('should test TimeBetweenDatesInDays and give correct return '){
+  });
+  it('should test TimeBetweenDatesInDays and give correct return ', function(){
     var startDate =new Date(1999, 4, 20, 0);
     var endDate=new Date(2000, 4, 20, 0);
-    expect(TimeBetween(startDate, endate)).toEqual(1*1000)
-  }
+    var TestSolarAge = new SolarAge(1918, 5, 25, 100);
+    expect(TestSolarAge.TimeBetweenDatesInDays(startDate, endDate)).toEqual(1*1000)
+  });
 
 });
